7ec571fdfee87d29c832141b7894de800ae6947c
Erin <erin.low95@gmail.com>
2017-10-05 12:45:57 -0700

    Completed MobilePhone and Contact Class.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,75 @@
+import java.util.Scanner;
 
 public class Main {
 
+	private static Scanner keyboard = new Scanner(System.in);
+	static MobilePhone mobilePhone = new MobilePhone();
 	public static void main(String[] args) {
+		boolean quit = false;
+		startPhone();
+		printMenu();
+		
+		while(!quit) {
+			System.out.print("What would you like to do [6 to show menu]: ");
+			int option = keyboard.nextInt();
+			keyboard.nextLine();
+			
+			switch(option) {
+			case 1: 
+				mobilePhone.printContacts();
+				break;
+			case 2:
+				addContact();
+				break;
+			case 3:
+				updateContact();
+				break;
+			case 4:
+				removeContact();
+				break;
+			case 5:
+				queryContact();
+				break;
+			case 6: 
+				printMenu();
+				break;
+			}
+		}
+		shutDown();
+	}
+	private static void queryContact() {
 		// TODO Auto-generated method stub
-
+		
+	}
+	private static void removeContact() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void updateContact() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void addContact() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void printMenu() {
+		System.out.println("Menu\n");
+		System.out.println("Press:\n" +
+				"0 - shut down\n" +
+				"1 - print contact list\n" +
+				"2 - add a new contact\n" +
+				"3 - update an existing contact\n" +
+				"4 - remove an existing contact\n" +
+				"5 - search for a contact" +
+				"6 - print menu");
+	}
+	private static void startPhone() {
+		System.out.println("Starting phone...");
+	}
+	
+	private static void shutDown() {
+		System.out.println("Shutting down...");
 	}
 
 }
--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -1,4 +1,89 @@
+import java.util.ArrayList;
 
 public class MobilePhone {
+	private ArrayList<Contact> contactList;
+	
+	public MobilePhone() {
+		this.contactList = new ArrayList<Contact>();
+	}
+	
+	// Method to add a new contact
+	public void addContact(Contact contact) {
+		if(search(contact) >= 0) {
+			System.out.println("Contact already exists");
+		}
+		else {
+			this.contactList.add(contact);
+			System.out.println("Contact was succefully added");
+		}
+	}
+	
+	// Method to update an existing contact
+	public void updateContact(Contact oldContact, Contact newContact) {
+		int index = search(oldContact);
+		if(index < 0) {
+			System.out.println("Contact does not exist");
+		}
+		else {
+			this.contactList.set(index, newContact);
+			System.out.println("Update succesful");
+		}
+	}
+	
+	// Method to remove an existing contact
+	public void removeContact(String name) {
+		if(search(name) >=0 ) {
+			this.contactList.remove(search(name));
+			System.out.print("Contact was removed");
+		}
+		else {
+			System.out.println("Contact does not exist.");
+		}
+		
+	}
+	
+	// Method to search for an existing name
+	public void queryContacts(String name) {
+		int index = search(name);
+		if(index >=0 ) {
+			Contact contact = this.contactList.get(index);
+			System.out.println("The phone number is: " + contact.getPhoneNumber());
+		}
+		else {
+			System.out.println("Contact does not exist.");
+		}
+	}
+	
+	// Method to print contact list 
+	public void printContacts() {
+		if(this.contactList.size() >0) {
+			System.out.println("Contact List:");
+			for(int i=0; i<this.contactList.size(); i++) {
+				Contact contact = this.contactList.get(i);
+				System.out.println(contact.getName() + " --> " + contact.getPhoneNumber());
+			}	
+		}
+		else {
+			System.out.println("You have no contacts in your contact list.");
+		}
+	}
 
+	// Search method that takes in Contact
+	private int search(Contact contact) {
+		return this.contactList.indexOf(contact);
+	}
+	
+	// Search method that takes in a String
+	private int search(String name) {
+		for(int i=0; i<this.contactList.size(); i++) {
+			Contact contact = this.contactList.get(i);
+			if(contact.getName().equals(name)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+
+	
 }
